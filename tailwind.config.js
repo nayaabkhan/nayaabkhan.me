267c1bfcae00eb2118e8af7057f6719d7ce9be2c
Nayaabkhan Khan <khannayaab@gmail.com>
2021-06-22 19:17:18 +0530

    Enable Tailwind JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  mode: 'jit',
   purge: {
     layers: [],
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
